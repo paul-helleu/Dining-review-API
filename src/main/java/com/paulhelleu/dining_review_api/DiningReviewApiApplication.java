ece40c6009dc6a019e5f5032e636b7db4cac8597
Paul HELLEU <57185255+paul-helleu@users.noreply.github.com>
2024-08-15 21:14:42 +0200

    init spring boot project

---
+++ src/main/java/com/paulhelleu/dining_review_api/DiningReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.paulhelleu.dining_review_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiningReviewApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/paulhelleu/dining_review_api/DiningReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulhelleu.dining_review_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
