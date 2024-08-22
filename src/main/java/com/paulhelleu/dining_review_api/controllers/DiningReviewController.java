94754ea8e4b79698ca16e9ad44fea5b4f68f151f
Paul HELLEU <57185255+paul-helleu@users.noreply.github.com>
2024-08-22 20:13:31 +0200

    feat: handle update and delete request

--- src/main/java/com/paulhelleu/dining_review_api/controllers/DiningReviewController.java
+++ src/main/java/com/paulhelleu/dining_review_api/controllers/DiningReviewController.java
@@ -8,7 +8,6 @@ import com.paulhelleu.dining_review_api.repositories.AdminReviewActionRepository
 import com.paulhelleu.dining_review_api.repositories.DiningReviewRepository;
 import com.paulhelleu.dining_review_api.repositories.RestaurantRepository;
 import com.paulhelleu.dining_review_api.repositories.UserRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -50,6 +49,42 @@ public class DiningReviewController {
     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
   }
 
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public DiningReview createDiningReview(@RequestBody DiningReview diningReview) {
+    if (diningReview == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dining review cannot be null");
+    }
+
+    String username = diningReview.getUsername();
+    if (username.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username cannot be empty");
+    }
+
+    Long restaurantId = diningReview.getRestaurantId();
+    if (restaurantId == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Restaurant id cannot be empty");
+    }
+
+    boolean restaurantExist = this.restaurantRepository.existsById(restaurantId);
+    if (!restaurantExist) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Restaurant not found");
+    }
+
+    boolean userExist = this.userRepository.existsByName(username);
+    if (!userExist) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, username + " not found");
+    }
+
+    AdminReviewAction adminReviewAction = new AdminReviewAction();
+    adminReviewAction.setApproveDiningReview(false);
+    adminReviewAction.setDiningReview(diningReview);
+
+    diningReview.setAdminStatus(adminReviewAction);
+
+    return this.diningReviewRepository.save(diningReview);
+  }
+
   @PutMapping("/review/{id}")
   public DiningReview updateDiningReview(@PathVariable Long id, @RequestBody DiningReview diningReviewUpdated) {
     Optional<DiningReview> diningReviewOptional = this.diningReviewRepository.findById(id);
@@ -80,7 +115,6 @@ public class DiningReviewController {
     return this.diningReviewRepository.findAll();
   }
 
-  @Transactional
   @DeleteMapping("/review/{id}")
   public DiningReview deleteDiningReview(@PathVariable Long id) {
     Optional<DiningReview> diningReviewOptional = this.diningReviewRepository.findById(id);
@@ -94,8 +128,8 @@ public class DiningReviewController {
   }
 
   // AdminReviewAction
-  @Transactional
   @PostMapping("/admin/approve/{id}")
+  @ResponseStatus(HttpStatus.CREATED)
   public AdminReviewAction approveReview(@PathVariable Long id, @RequestBody Boolean status) {
     if (status == null) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Status cannot be empty");
@@ -114,6 +148,7 @@ public class DiningReviewController {
     return this.adminReviewActionRepository.save(adminReviewAction);
   }
 
+  // User
   @GetMapping("/users/{name}")
   public User getUser(@PathVariable String name) {
     Optional<User> userOptional = this.userRepository.findByName(name);
@@ -124,6 +159,70 @@ public class DiningReviewController {
     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
   }
 
+  @PostMapping("/users")
+  @ResponseStatus(HttpStatus.CREATED)
+  public User createUser(@RequestBody User user) {
+    if (user == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User cannot be null");
+    }
+    if (user.getName() == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username cannot be empty");
+    }
+
+    Boolean userExist = this.userRepository.existsByName(user.getName());
+    if (userExist) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists");
+    }
+
+    return this.userRepository.save(user);
+  }
+
+  @PutMapping("/users/{name}")
+  public User updateUser(@PathVariable String name, @RequestBody User userUpdated) {
+    Optional<User> userOptional = this.userRepository.findByName(name);
+    if (userOptional.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    if (userUpdated == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User cannot be null");
+    }
+
+    User user = userOptional.get();
+    if (userUpdated.getCity() != null) {
+      user.setCity(userUpdated.getCity());
+    }
+    if (userUpdated.getState() != null) {
+      user.setState(userUpdated.getState());
+    }
+    if (userUpdated.getZipcode() != null) {
+      user.setZipcode(userUpdated.getZipcode());
+    }
+    if (userUpdated.getDairyAllergy() != null) {
+      user.setDairyAllergy(userUpdated.getDairyAllergy());
+    }
+    if (userUpdated.getEggAllergy() != null) {
+      user.setPeanutAllergy(userUpdated.getPeanutAllergy());
+    }
+    if (userUpdated.getPeanutAllergy() != null) {
+      user.setPeanutAllergy(userUpdated.getPeanutAllergy());
+    }
+
+    return user;
+  }
+
+  @DeleteMapping("/users/{name}")
+  public User deleteUser(@PathVariable String name) {
+    Optional<User> userOptional = this.userRepository.findByName(name);
+    if (userOptional.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    User user = userOptional.get();
+    this.userRepository.delete(user);
+    return user;
+  }
+
+  // Restaurant
   @GetMapping("/restaurants/{id}")
   public Restaurant getRestaurant(@PathVariable Long id) {
     Optional<Restaurant> restaurantOptional = this.restaurantRepository.findById(id);
@@ -166,75 +265,52 @@ public class DiningReviewController {
       throw new ResponseStatusException(HttpStatus.CONFLICT, "Restaurant with the same name and zip code already exists");
     }
 
-    try {
-      return this.restaurantRepository.save(restaurant);
-    } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-    }
+    return this.restaurantRepository.save(restaurant);
   }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public DiningReview createDiningReview(@RequestBody DiningReview diningReview) {
-    if (diningReview == null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dining review cannot be null");
+  @PutMapping("/restaurants/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Restaurant updateRestaurant(@PathVariable Long id, @RequestBody Restaurant updatedRestaurant) {
+    Optional<Restaurant> restaurantOptional = this.restaurantRepository.findById(id);
+    if (restaurantOptional.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
-    String username = diningReview.getUsername();
-    if (username.isEmpty()) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username cannot be empty");
+    Restaurant restaurant = restaurantOptional.get();
+    if (updatedRestaurant.getName() != null) {
+      restaurant.setName(updatedRestaurant.getName());
+    }
+    if (updatedRestaurant.getZipCode() != null) {
+      restaurant.setZipCode(updatedRestaurant.getZipCode());
+    }
+    if (updatedRestaurant.getDairyAllergy() != null) {
+      restaurant.setDairyAllergy(updatedRestaurant.getDairyAllergy());
+    }
+    if (updatedRestaurant.getEggAllergy() != null) {
+      restaurant.setEggAllergy(updatedRestaurant.getEggAllergy());
+    }
+    if (updatedRestaurant.getPeanutAllergy() != null) {
+      restaurant.setPeanutAllergy(updatedRestaurant.getPeanutAllergy());
+    }
+    if (updatedRestaurant.getOverallScore() != null) {
+      restaurant.setOverallScore(updatedRestaurant.getOverallScore());
     }
 
-    Long restaurantId = diningReview.getRestaurantId();
-    if (restaurantId == null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Restaurant id cannot be empty");
-    }
-
-    boolean restaurantExist = this.restaurantRepository.existsById(restaurantId);
-    if (!restaurantExist) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Restaurant not found");
-    }
-
-    boolean userExist = this.userRepository.existsByName(username);
-    if (!userExist) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, username + " not found");
-    }
-
-    AdminReviewAction adminReviewAction = new AdminReviewAction();
-    adminReviewAction.setApproveDiningReview(false);
-    adminReviewAction.setDiningReview(diningReview);
-
-    diningReview.setAdminStatus(adminReviewAction);
-
-    try {
-      return this.diningReviewRepository.save(diningReview);
-    } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-    }
+    return restaurantRepository.save(restaurant);
   }
 
-  @PostMapping("/users")
-  @ResponseStatus(HttpStatus.CREATED)
-  public User createUser(@RequestBody User user) {
-    if (user == null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User cannot be null");
-    }
-    if (user.getName() == null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username cannot be empty");
+  @DeleteMapping("/restaurants/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Restaurant deleteRestaurant(@PathVariable Long id) {
+    Optional<Restaurant> restaurantOptional = this.restaurantRepository.findById(id);
+    if (restaurantOptional.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
-    Boolean userExist = this.userRepository.existsByName(user.getName());
-    if (userExist) {
-      throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists");
-    }
+    Restaurant restaurant = restaurantOptional.get();
+    this.restaurantRepository.delete(restaurant);
 
-    try {
-      return this.userRepository.save(user);
-    } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-    }
+    return restaurant;
   }
-
-
 
 }
